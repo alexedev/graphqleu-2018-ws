d48e6df512c915416df56d29f1b81504a4bb3cea
Alex Alexeev <work2alex@gmail.com>
2018-06-14 12:44:22 +0200

    Add prisma binding queries

--- server/index.js
+++ server/index.js
@@ -2,9 +2,28 @@ const { Prisma } = require('prisma-binding');
 
 const prisma = new Prisma({
   typeDefs: 'generated/prisma.graphql',
-  endpoint: '__YOUR_PRISMA_ENDPOINT__'
+  endpoint: 'https://eu1.prisma.sh/alex-alexeev/gqlws/dev'
 });
 
 prisma.mutation
-  .createUser({ data: { name: 'Alex New' } }, '{ id name }')
+  .createUser({ data: { name: 'Alex 7' } }, '{ id name }')
+  .then(response =>
+    prisma.mutation.createPost({
+      data: {
+        title: `Post by ${response.name}`,
+        content: 'text here',
+        author: {
+          connect: {
+            id: response.id
+          }
+        }
+      }
+    })
+  )
+  .then(console.log)
+  .then(() => prisma.query.users(null, '{id name posts {title}}'))
+  .then(console.log)
+  .then(() => prisma.mutation.deleteManyPosts())
+  .then(console.log)
+  .then(() => prisma.mutation.deleteManyUsers())
   .then(console.log);
